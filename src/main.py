5b8b703dc206a6887886e6069f204b59ec358bfb
Ramakurama <bidjilob2000@gmail.com>
2025-10-14 18:51:45 +0300

    Its my first commit

---
+++ src/main.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+import argparse
+import sys
+
+from CalcBStudents import CalcBStudents
+from CalcRating import CalcRating
+from XmlDataReader import XmlDataReader
+
+
+def get_path_from_arguments(args) -> str:
+    parser = argparse.ArgumentParser(description="Path to datafile")
+    parser.add_argument("-p", dest="path", type=str, required=True,
+                        help="Path to datafile")
+    args = parser.parse_args(args)
+    return args.path
+
+
+def main():
+    path = get_path_from_arguments(sys.argv[1:])
+
+    reader = XmlDataReader()
+    students = reader.read(path)
+    print("Students: ", students)
+
+    rating = CalcRating(students).calc()
+    print("Rating: ", rating)
+
+    b_student_count = CalcBStudents(students).calc()
+    print("Студентов хорошистов (имеющих баллы по всем предметам >= 76): ", b_student_count)
+
+
+if __name__ == "__main__":
+    main()
